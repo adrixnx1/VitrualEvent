26959e91198b5ad9c995dd7849eef6f90e8848ab
MexicanMochi <alantello1230@gmail.com>
2025-04-07 18:45:20 -0500

    Alan's Update

---
+++ VirtualEvent_DB/dbo/Tables/Review.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Review] (
+    [ReviewID]   INT             NOT NULL,
+    [UserID]     INT             NOT NULL,
+    [TripID]     INT             NOT NULL,
+    [Rating]     INT             NOT NULL,
+    [Comment]    NVARCHAR (1000) NOT NULL,
+    [ReviewDate] DATETIME        NOT NULL,
+    CONSTRAINT [PK_Review] PRIMARY KEY CLUSTERED ([ReviewID] ASC),
+    CONSTRAINT [FK_Review_UserID] FOREIGN KEY ([ReviewID]) REFERENCES [dbo].[UserID] ([UserID])
+);
+
---
+++ VirtualEvent_DB/dbo/Tables/TripCollaboration.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TripCollaboration] (
+    [CollabID]     INT           NOT NULL,
+    [TripID]       INT           NOT NULL,
+    [UserID]       INT           NOT NULL,
+    [Role]         NVARCHAR (50) NOT NULL,
+    [InviteStatus] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_TripCollaboration] PRIMARY KEY CLUSTERED ([CollabID] ASC),
+    CONSTRAINT [FK_TripCollaboration_UserID] FOREIGN KEY ([CollabID]) REFERENCES [dbo].[UserID] ([UserID])
+);
+
