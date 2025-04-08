1d74584f89d3b3b40c0f9387f756eb933136f051
MexicanMochi <alantello1230@gmail.com>
2025-04-07 19:18:56 -0500

    Project Demo #4

---
+++ VirtualEvent_WEB/Models/TripStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VirtualEvent_WEB.Models
+{
+    public static class TripStore
+    {
+        public static List<Trip> Trips { get; set; } = new List<Trip>();
+    }
+
+    public class Trip
+    {
+        public string TripName { get; set; }
+        public string Country { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
