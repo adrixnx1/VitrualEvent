878d416de5c29d4de0570d2d4bf28ba8205dedf8
Adriana Barcenas <abarcenas2020@gmail.com>
2025-04-13 22:35:59 -0500

    this is the one

--- VirtualEvent_WEB/Model/Registration.cs
+++ VirtualEvent_WEB/Model/Registration.cs
@@ -1,27 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace VirtualEvent_WEB.Model
 {
     public class Registration
     {
+        [Required(ErrorMessage = "First name is required.")]
         [Display(Name = "First Name")]
-        [Required(ErrorMessage = "First Name is required")]
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last name is required.")]
         [Display(Name = "Last Name")]
-        [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address.")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "Password must be at least 10 characters long.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
-
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
         [Display(Name = "Confirm Password")]
-        [Required(ErrorMessage = "Confirm Password is required")]
         public string ConfirmPassword { get; set; }
-        public DateTime RegistrationDate { get; set; }
+
+        [Display(Name = "Registration Date")]
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
     }
 }
